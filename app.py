0223d7a602b1e7b8a0bd485296a02ecda3f65e03
RicardoTizzano <ricardotizzano@gmail.com>
2023-12-21 12:12:14 +0100

    Se corrige /CV

--- app.py
+++ app.py
@@ -8,10 +8,5 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/cv')
-def cv():
-    return render_template('cv.html')
-
-
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=8000)
